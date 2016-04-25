0be56de0caecfb57f0a70106d3e54ff2a7bdff0d
Max Semenik <maxsem.wiki@gmail.com>
2016-04-25 14:55:12 -0700

    This is not FooBar :)
    Change-Id: Id6935fc28486672e15a492308ee332ed1b2a62ca

--- Disambiguator.php
+++ Disambiguator.php
@@ -12,5 +12,5 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	); */
 	return;
 } else {
-	die( 'This version of the FooBar extension requires MediaWiki 1.25+' );
+	die( 'This version of the Disambiguator extension requires MediaWiki 1.25+' );
 }
